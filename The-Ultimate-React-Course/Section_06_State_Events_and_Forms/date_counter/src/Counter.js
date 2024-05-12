efd9d6fee666de97ac8512d466ec11d9794fddc0
GregWagner <theGreg101@gmail.com>
2024-05-12 08:24:38 -0500

    working on state

---
+++ The-Ultimate-React-Course/Section_06_State_Events_and_Forms/date_counter/src/Counter.js
@@ -0,0 +1,37 @@
+import React, { component, useState } from 'react';
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(5);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((step) => step - 1)}>-</button>
+        <span> Step: {step} </span>
+        <button onClick={() => setStep((step) => step + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((count) => count - step)}>-</button>
+        <span> Count: {count} </span>
+        <button onClick={() => setCount((count) => count + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? 'Today is '
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
+
+export default Counter;
